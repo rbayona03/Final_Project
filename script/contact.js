af500c1e3439bb7f4f4fb13333c2a1c5e5f5dc20
Roberto Bayona <rbayona03@gmail.com>
2021-10-06 23:57:52 -0400

    email validation pt.1

--- script/contact.js
+++ script/contact.js
@@ -8,6 +8,12 @@ const formSubmit = document.querySelector('.submitMsg');
 let isFormValid = false;
 let validationOn = false;
 
+const validateEmail = (email)  => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
+validateEmail(email)
+
 const resetInput = (el) => {
     el.classList.remove('invalid');
     el.nextElementSibling.classList.add('hiddenMsg');
